7bdb657671b16113095563ce276ae6c38e97c664
Ingrid Yiu <yiui135@newschool.edu>
2017-05-05 09:46:51 -0400

    new disqus

--- p2_final/assets/js/main.js
+++ p2_final/assets/js/main.js
@@ -1,1 +1,39 @@
-$('#block').css('background-color','red');
+
+// ---- Top Navigation ----
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// ---- Index Slideshow ----
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("blogpreview");
+  var dots = document.getElementsByClassName("buttonno");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-red", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+}	
